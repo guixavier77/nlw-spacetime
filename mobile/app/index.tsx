5cf447c21dadf579484e07569992b2c7ec9cfdab
Guilherme Xavier <xguilherme1@gmail.com>
2023-05-27 00:02:52 -0300

    authentication middleware configuration

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,28 +1,72 @@
+import React from 'react'
+import { useRouter } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import * as SecureStore from 'expo-secure-store'
+import { styled } from 'nativewind'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { api } from '../src/lib/api'
+
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWlogo from './src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWlogo from '../src/assets/nlw-spacetime-logo.svg'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/13f9472dfb6a9d8cc978',
+}
+
 export default function App() {
+  const router = useRouter()
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
 
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '13f9472dfb6a9d8cc978',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('memories')
+  }
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
+
   if (!hasLoadedFonts) {
     return null
   }
+
   return (
     <ImageBackground
       source={blurBg}
@@ -45,6 +89,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { getUser } from '@/lib/auth'
 import Image from 'next/image'
 
@@ -17,7 +15,10 @@ const Profile = () => {
       />
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
-        <a href="" className="block text-red-400 hover:text-red-300">
+        <a
+          href="/api/auth/logout/"
+          className="block text-red-400 hover:text-red-300"
+        >
           Quero sair
         </a>
       </p>
